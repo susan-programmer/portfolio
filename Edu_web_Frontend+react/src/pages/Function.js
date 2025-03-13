f78d94e8e9ddd1624c1e5ae18a37edba0cbe1736
safdari <67308738+susan-programmer@users.noreply.github.com>
2025-03-13 10:46:02 +0330

    Add files via upload

---
+++ Edu_web_Frontend+react/src/pages/Function.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Function() {
+  return (
+    <div className="container text-center py-5">
+      <h1 className="fw-bold text-primary">عملکرد مغز</h1>
+      <p>مغز، بیشتر فعالیت‌های بدن را کنترل می‌کند. به طور کلی این ارگان، اطلاعاتی را که از اندام‌های حسی دریافت و سپس پردازش، یکپارچه‌سازی و هماهنگ می‌کند 
+        و در نهایت در مورد دستورالعمل‌های ارسال شده به بقیه بدن تصمیم می‌گیرد. مغز درون ساختمان جمجمه انسان قرار گرفته و توسط آن محافظت می‌شود.</p>
+      <img
+        src="/brain.webp"
+        alt="عملکرد مغز"
+        className="img-fluid rounded shadow-lg mt-4"
+        width="400px"
+      />
+    </div>
+  );
+}
+
+export default Function;
